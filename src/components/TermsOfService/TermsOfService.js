34f9bec2e5cb27336311edd80396a0a591b35126
v4vian <47902761+v4vian@users.noreply.github.com>
2019-04-04 16:19:52 -0700

    Update TermsOfService.js

--- src/components/TermsOfService/TermsOfService.js
+++ src/components/TermsOfService/TermsOfService.js
@@ -11,22 +11,46 @@ const TermsOfService = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
-
+      <p className={css.lastUpdated}>Last updated: April 2, 2019</p>
+    
+      <h2>1 AGREEMENT TO TERMS</h2>
       <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        These Terms of Use constitute a legally binding agreement made between you, whether personally 
+        or on behalf of an entity (“you”) and Glamp Oasis (“we,” “us” or “our”), concerning your access to 
+        and use of the glampoasis.com website as well as any other media form, media channel, mobile website 
+        or mobile application related, linked, or otherwise connected thereto (collectively, the “Glamp Oasis”). 
       </p>
-
-      <h2>1 Lorem ipsum dolor sit amet</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        You agree that by accessing Glamp Oasis, you have read, understood, and agree to be bound by all of these 
+        Terms of Use. If you do not agree with all of these Terms of Use, then you are expressly prohibited from 
+        using Glamp Oasis and you must discontinue use immediately. 
+      </p>
+      <p>
+        Supplemental terms and conditions or documents that may be posted on Glamp Oasis from time to time are 
+        hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make 
+        changes or modifications to these Terms of Use at any time and for any reason.
+      </p>
+      <p>
+        We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive 
+        any right to receive specific notice of each such change. 
+      </p>
+      <p>
+        It is your responsibility to periodically review these Terms of Use to stay informed of updates. You will be 
+        subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms 
+        of Use by your continued use of Glamp Oasis after the date such revised Terms of Use are posted. 
+      </p>
+      <p>
+        The information provided on Glamp Oasis is not intended for distribution to or use by any person or entity in 
+        any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would 
+        subject us to any registration requirement within such jurisdiction or country. 
+      </p>
+      <p>
+        Accordingly, those persons who choose to access Glamp Oasis from other locations do so on their own initiative 
+        and are solely responsible for compliance with local laws, if and to the extent local laws are applicable. 
+      </p>
+      <p>
+        Glamp Oasis is intended for users who are at least 18 years old. Persons under the age of 18 are not permitted 
+        to use or register for Glamp Oasis. 
       </p>
 
       <h2>2 Sed ut perspiciatis unde</h2>
